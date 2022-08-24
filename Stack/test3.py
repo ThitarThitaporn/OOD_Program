a3eec58ba9564e669b12c970075cfa2de567cca1
ThitarThitaporn <64011100@KMITL.AC.TH>
2022-08-24 23:35:12 +0700

    stack

--- Stack/test3.py
+++ Stack/test3.py
@@ -1,11 +1,49 @@
-str1, str2 = input('Enter Input :').split(',')
-class Stack :
-    def __init__(self, list = None) :
-        if list == None :
-            self.items = []
+from pickle import DUP
+
+
+class StackCalc:
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return len(self.items) == 0
+    
+    def size(self):
+        return len(self.items)
+    
+    def peek(self):
+        return self.items[-1]
+
+    def push(self,tmp):
+        self.items.append(tmp)
+    
+    def pop(self):
+        return self.items.pop()
+
+def run(st):
+    s = StackCalc()
+    for i in st:
+        if i=="+":s.push(s.pop()+s.pop())
+        elif i=="-":s.push(-1*s.pop()+s.pop())
+        elif i=="*":s.push(s.pop()*s.pop())
+        elif i=="/":
+            a=s.pop()
+            b=s.pop()
+            s.push(b/a)
+
+        elif i== 'DUP' :
+            s.peek(i)
+           #print ('1')
+        elif i== 'POP' :
+            s.pop(i)
+        elif i== 'PSH' :
+            s.push(i)
+
         else:
-            self.item = list
+           s.push()
+           
+    return s.pop()
 
-
- elif(int(S.peek())%2==0 ):
-        st.push(int(S.pop())-1)
+print("* Stack Calculator *")
+arg = list(input("Enter arguments : ").split())
+print(int(run(arg)))
